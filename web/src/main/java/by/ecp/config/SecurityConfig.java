5d49a2395c2385d043486074e1553edb9864376b
Vintyk <vinty1978@gmail.com>
2017-06-30 15:24:44 +0300

    secur + html

--- web/src/main/java/by/ecp/config/SecurityConfig.java
+++ web/src/main/java/by/ecp/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/GameProject", "/login")
+                .antMatchers("/login")
                 .permitAll()
                 .antMatchers("/adminTool")
                 .hasAuthority("Admin")
